e9a6754d721ed86a6a38f983202ec3256a742b92
Anton Österberg <anton.osterberg@gmail.com>
2015-05-09 15:36:46 +0200

    Nu kan vi äntligen testa testfall med Android-bibliotek via Robolectric!

---
+++ src/diacheck/libs/analytes/Analyte.java
@@ -0,0 +1,7 @@
+package diacheck.libs.analytes;
+
+public interface Analyte
+{
+	public double result();
+	public byte zone();
+}
---
+++ src/diacheck/libs/analytes/SpecificGravity.java
@@ -0,0 +1,20 @@
+package diacheck.libs.analytes;
+
+public class SpecificGravity implements Analyte
+{
+
+	@Override
+	public double result()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public byte zone()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
