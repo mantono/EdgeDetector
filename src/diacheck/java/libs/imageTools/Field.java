616c71971c34986fd33adae45cbb682fcd0e000f
Anton Österberg <anton.osterberg@gmail.com>
2015-10-15 19:43:43 +0200

    - Added JavaDocs to class Field. - Deleted test class FieldTypeTest since is no longer usable in its   current form after changes in FieldType.

--- src/diacheck/java/libs/imageTools/Field.java
+++ src/diacheck/java/libs/imageTools/Field.java
@@ -5,11 +5,22 @@ import java.awt.Color;
 import java.util.List;
 import java.util.Arrays;
 
+/**
+ * Represents a field from the sample image, containing pixel data for entire field. 
+ * @author Anton &Ouml;sterberg
+ *
+ */
 public class Field
 {
 	private final List<Color> pixels;
 	private final FieldType fieldtype;
 	
+	/**
+	 * Public constructor for <code>Field</code>
+	 * 
+	 * @param type describes which type of field the instance represents.
+	 * @param pixels contains the color data for the fields pixels.
+	 */
 	public Field(FieldType type, final List<Color> pixels)
 	{
 		this.pixels = pixels;
@@ -18,21 +29,39 @@ public class Field
 			throw new IllegalArgumentException("The amount of pixels must be at least one!");
 	}
 	
+	/**
+	 * Public constructor for <code>Field</code>
+	 * 
+	 * @param type describes which type of field the instance represents.
+	 * @param pixels contains the color data for the fields pixels.
+	 */
 	public Field(FieldType type, final Color[] pixels)
 	{
 		this(type, (List<Color>) Arrays.asList(pixels));
 	}
 	
+	/**
+	 * 
+	 * @return returns the size of the field measured in pixels.
+	 */
 	public int getAmountOfPixels()
 	{
 		return pixels.size();
 	}
 	
+	/**
+	 * 
+	 * @return returns the <code>FieldType</code> representing the instance of the field.
+	 */
 	public FieldType getFieldType()
 	{
 		return this.fieldtype;
 	}
 
+	/**
+	 * 
+	 * @return returns a <code>Color</code> object, representing the average color of all pixels in the field.
+	 */
 	public Color getAverageColor()
 	{
 		int red, green, blue;
